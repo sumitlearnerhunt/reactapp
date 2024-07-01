05690237c4c10911e4f6f1542cb46812b1903940
Sumit Rawat <sumitr@learnerhunt.com>
2024-07-01 16:54:57 +0530

    3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Sumittt Rawat the great
+          Learn React Sumittt Rawat the great the legend
         </a>
       </header>
     </div>
